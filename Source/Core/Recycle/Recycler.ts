ce2273de6b7e53b1686313a64805385752c5a8a4
Jonas Plotzky <plonjo3@gmail.com>
2024-10-02 18:05:28 +0200

    - Use RecyclableArray in recycler - Remove borrow - micro optimisations

--- Source/Core/Recycle/Recycler.ts
+++ Source/Core/Recycle/Recycler.ts
@@ -17,19 +17,19 @@ namespace FudgeCore {
    * @link https://github.com/hs-furtwangen/FUDGE/wiki/Recycler
    */
   export abstract class Recycler {
-    private static depot: { [type: string]: Object[] } = {};
+    private static depot: { [type: string]: RecycableArray<Object> } = {};
 
     /**
      * Fetches an object of the requested type from the depot, calls its recycle-method and returns it.
      * If the depot for that type is empty it returns a new object of the requested type.
      * @param _t The class identifier of the desired object
      */
-    public static get<T extends Recycable | RecycableArray<T>>(_t: new () => T): T {
+    public static get<T extends Recycable | RecycableArray<T> | Object>(_t: new () => T): T {
       let key: string = _t.name;
-      let instances: Object[] = Recycler.depot[key];
+      let instances: RecycableArray<Object> = Recycler.depot[key];
       if (instances && instances.length > 0) {
         let instance: T = <T>instances.pop();
-        instance.recycle();
+        (<Recycable>instance).recycle?.();
         return instance;
       } else
         return new _t();
@@ -44,37 +44,18 @@ namespace FudgeCore {
     }
 
     /**
-     * Returns a reference to an object of the requested type in the depot, but does not remove it there. 
-     * If no object of the requested type was in the depot, one is created, stored and borrowed.
-     * For short term usage of objects in a local scope, when there will be no other call to {@link Recycler.get}, {@link Recycler.reuse} or {@link Recycler.borrow}!
-     * @param _t The class identifier of the desired object
-     */
-    public static borrow<T extends Recycable>(_t: new () => T): T {
-      let t: T;
-      let key: string = _t.name;
-      let instances: Object[] = Recycler.depot[key];
-      if (!instances || instances.length == 0) {
-        t = new _t();
-        Recycler.store(t);
-        return t;
-      }
-      let instance: T = <T>instances[0];
-      instance.recycle();
-      return instance;
-    }
-
-    /**
      * Stores the object in the depot for later recycling. Users are responsible for throwing in objects that are about to loose scope and are not referenced by any other
      * @param _instance
      */
     public static store(_instance: Object): void {
-      let key: string = _instance.constructor.name;
-      //Debug.log(key);
-      let instances: Object[] = Recycler.depot[key] || [];
-      instances.push(_instance);
-      Recycler.depot[key] = instances;
+      // let key: string = _instance.constructor.name;
+      // //Debug.log(key);
+      // let instances: RecycableArray<Object> = RecyclerNew.depot[key] || new RecycableArray<Object>();
+      // instances.push(_instance);
+      // RecyclerNew.depot[key] = instances;
       // Debug.log(`ObjectManager.depot[${key}]: ${ObjectManager.depot[key].length}`);
       //Debug.log(this.depot);
+      (Recycler.depot[_instance.constructor.name] ??= new RecycableArray<Object>()).push(_instance);
     }
 
     /**
@@ -90,8 +71,7 @@ namespace FudgeCore {
      * @param _t
      */
     public static dump<T>(_t: new () => T): void {
-      let key: string = _t.name;
-      Recycler.depot[key] = [];
+      Recycler.depot[_t.name] = new RecycableArray<Object>();
     }
 
     /**
