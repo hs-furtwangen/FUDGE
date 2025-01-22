d882e71a0f393ada66876c5801d5219f6042b4c4
plojo <plonjo3@gmail.com>
2025-01-22 16:33:55 +0100

    refactor render prepare into two functions

--- Source/Core/Render/Render.ts
+++ Source/Core/Render/Render.ts
@@ -20,29 +20,73 @@ namespace FudgeCore {
     private static readonly modificationsProcessed: Set<{ modified: boolean }> = new Set();
     private static timestampUpdate: number;
 
-    //#region Prepare
     /**
      * Recursively iterates over the branch starting with the node given, recalculates all world transforms, 
      * collects all lights and feeds all shaders used in the graph with these lights. Sorts nodes for different
      * render passes.
+     * @param _recalculate - set true to force recalculation of all world transforms in the given branch, even if their local transforms haven't changed
      */
     @PerformanceMonitor.measure("Render.prepare")
-    public static prepare(_branch: Node, _options: RenderPrepareOptions = {}, _mtxWorld: Matrix4x4 = Matrix4x4.IDENTITY(), _shadersUsed: (ShaderInterface)[] = null, _recalculateTransforms?: boolean): void {
-      let firstLevel: boolean = (_shadersUsed == null);
-      if (firstLevel) {
-        _shadersUsed = [];
-        _recalculateTransforms = false;
-        Render.timestampUpdate = performance.now();
-        Render.nodesSimple.reset();
-        Render.nodesAlpha.reset();
-        Render.nodesPhysics.reset();
-        Render.componentsPick.reset();
-        Render.componentsSkeleton.reset();
-        Render.modificationsProcessed.clear();
-        Render.lights.forEach(_array => _array.reset());
-        _branch.dispatchEvent(new Event(EVENT.RENDER_PREPARE_START));
-      }
+    public static prepare(_branch: Node, _options: RenderPrepareOptions = {}, _mtxWorld: Matrix4x4 = Matrix4x4.IDENTITY(), _recalculate: boolean = false): void {
+      Render.timestampUpdate = performance.now();
+      Render.nodesSimple.reset();
+      Render.nodesAlpha.reset();
+      Render.nodesPhysics.reset();
+      Render.componentsPick.reset();
+      Render.componentsSkeleton.reset();
+      Render.modificationsProcessed.clear();
+      Render.lights.forEach(_array => _array.reset());
+      _branch.dispatchEvent(new Event(EVENT.RENDER_PREPARE_START));
 
+      this.prepareBranch(_branch, _options, _mtxWorld, _recalculate);
+
+      for (const processed of Render.modificationsProcessed)
+        processed.modified = false;
+      _branch.dispatchEvent(new Event(EVENT.RENDER_PREPARE_END));
+      for (const cmpSkeleton of Render.componentsSkeleton) {
+        cmpSkeleton.update();
+        cmpSkeleton.updateRenderBuffer();
+      }
+      Render.bufferLights(Render.lights);
+    }
+
+    public static addLights(_cmpLights: ComponentLight[]): void {
+      for (let cmpLight of _cmpLights) {
+        if (!cmpLight.isActive)
+          continue;
+
+        let type: TypeOfLight = cmpLight.light.getType();
+        let lightsOfType: RecycableArray<ComponentLight> = Render.lights.get(type);
+        if (!lightsOfType) {
+          lightsOfType = new RecycableArray<ComponentLight>();
+          Render.lights.set(type, lightsOfType);
+        }
+        lightsOfType.push(cmpLight);
+      }
+    }
+
+    /**
+     * Used with a {@link Picker}-camera, this method renders one pixel with picking information 
+     * for each node in the line of sight and return that as an unsorted {@link Pick}-array
+     */
+    public static pick(_nodes: Node[], _cmpCamera: ComponentCamera): Pick[] { // TODO: see if third parameter _world?: Matrix4x4 would be usefull
+      return Render.pickFrom(_nodes, _cmpCamera, super.pick);
+    }
+
+    /**
+     * Draws the scene from the point of view of the given camera
+     */
+    @PerformanceMonitor.measure("Render.draw")
+    public static draw(_cmpCamera: ComponentCamera): void {
+      for (let node of Render.nodesAlpha)
+        Reflect.set(node, "zCamera", _cmpCamera.pointWorldToClip(node.getComponent(ComponentMesh).mtxWorld.translation).z);
+
+      const sorted: Node[] = Render.nodesAlpha.getSorted((_a: Node, _b: Node) => Reflect.get(_b, "zCamera") - Reflect.get(_a, "zCamera"));
+
+      Render.drawNodes(Render.nodesSimple, sorted, _cmpCamera);
+    }
+
+    private static prepareBranch(_branch: Node, _options: RenderPrepareOptions, _mtxWorld: Matrix4x4, _recalculate: boolean): void {
       if (!_branch.isActive)
         return; // don't add branch to render list if not active
 
@@ -54,7 +98,7 @@ namespace FudgeCore {
 
       let cmpTransform: ComponentTransform = _branch.getComponent(ComponentTransform);
       if (cmpTransform && cmpTransform.isActive) {
-        if ((_recalculateTransforms ||= cmpTransform.mtxLocal.modified)) {
+        if ((_recalculate ||= cmpTransform.mtxLocal.modified)) {
           PerformanceMonitor.startMeasure("Render.prepare mtxWorld * mtxLocal");
           let mtxWorldBranch: Matrix4x4 = Matrix4x4.PRODUCT(_mtxWorld, _branch.cmpTransform.mtxLocal);
           PerformanceMonitor.endMeasure("Render.prepare mtxWorld * mtxLocal");
@@ -94,13 +138,6 @@ namespace FudgeCore {
           Render.modificationsProcessed.add(_branch.mtxWorld);
         }
 
-        let shader: ShaderInterface = cmpMaterial.material.getShader();
-        let cmpParticleSystem: ComponentParticleSystem = _branch.getComponent(ComponentParticleSystem);
-        if (cmpParticleSystem && cmpParticleSystem.isActive && cmpParticleSystem.particleSystem != null)
-          shader = cmpParticleSystem.particleSystem.getShaderFrom(shader);
-        if (_shadersUsed.indexOf(shader) < 0)
-          _shadersUsed.push(shader);
-
         _branch.radius = cmpMesh.radius;
         if (cmpMaterial.sortForAlpha || _branch.getComponent(ComponentText)) // always sort text for alpha
           Render.nodesAlpha.push(_branch); // add this node to render list
@@ -114,7 +151,7 @@ namespace FudgeCore {
           Render.componentsSkeleton.push(cmpSkeleton);
 
       for (let child of _branch.getChildren()) {
-        Render.prepare(child, _options, _branch.mtxWorld, _shadersUsed, _recalculateTransforms);
+        Render.prepareBranch(child, _options, _branch.mtxWorld, _recalculate);
 
         _branch.nNodesInBranch += child.nNodesInBranch;
         let cmpMeshChild: ComponentMesh = child.getComponent(ComponentMesh);
@@ -123,62 +160,8 @@ namespace FudgeCore {
         _branch.radius = Math.max(_branch.radius, position.getDistance(_branch.mtxWorld.translation) + child.radius);
         Recycler.store(position);
       }
-
-      if (firstLevel) {
-        for (const processed of Render.modificationsProcessed)
-          processed.modified = false;
-        
-        _branch.dispatchEvent(new Event(EVENT.RENDER_PREPARE_END));
-        for (const cmpSkeleton of Render.componentsSkeleton) {
-          cmpSkeleton.update();
-          cmpSkeleton.updateRenderBuffer();
-        }
-        Render.bufferLights(Render.lights);
-      }
     }
 
-    public static addLights(_cmpLights: ComponentLight[]): void {
-      for (let cmpLight of _cmpLights) {
-        if (!cmpLight.isActive)
-          continue;
-
-        let type: TypeOfLight = cmpLight.light.getType();
-        let lightsOfType: RecycableArray<ComponentLight> = Render.lights.get(type);
-        if (!lightsOfType) {
-          lightsOfType = new RecycableArray<ComponentLight>();
-          Render.lights.set(type, lightsOfType);
-        }
-        lightsOfType.push(cmpLight);
-      }
-    }
-    //#endregion
-
-    //#region Picking
-    /**
-     * Used with a {@link Picker}-camera, this method renders one pixel with picking information 
-     * for each node in the line of sight and return that as an unsorted {@link Pick}-array
-     */
-    public static pick(_nodes: Node[], _cmpCamera: ComponentCamera): Pick[] { // TODO: see if third parameter _world?: Matrix4x4 would be usefull
-      return Render.pickFrom(_nodes, _cmpCamera, super.pick);
-    }
-    //#endregion
-
-    //#region Drawing
-    /**
-     * Draws the scene from the point of view of the given camera
-     */
-    @PerformanceMonitor.measure("Render.draw")
-    public static draw(_cmpCamera: ComponentCamera): void {
-      for (let node of Render.nodesAlpha)
-        Reflect.set(node, "zCamera", _cmpCamera.pointWorldToClip(node.getComponent(ComponentMesh).mtxWorld.translation).z);
-
-      const sorted: Node[] = Render.nodesAlpha.getSorted((_a: Node, _b: Node) => Reflect.get(_b, "zCamera") - Reflect.get(_a, "zCamera"));
-
-      Render.drawNodes(Render.nodesSimple, sorted, _cmpCamera);
-    }
-    //#endregion
-
-    //#region Physics
     private static transformByPhysics(_node: Node, _cmpRigidbody: ComponentRigidbody): void {
       if (!_cmpRigidbody.isInitialized) // || Project.mode == MODE.EDITOR)
         _cmpRigidbody.initialize();
@@ -212,8 +195,5 @@ namespace FudgeCore {
       Recycler.store(mtxWorld);
       Recycler.store(mtxLocal);
     }
-    //#endregion
-
-
   }
 }
