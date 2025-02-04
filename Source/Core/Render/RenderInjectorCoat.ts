d11563ac1f6a62393e55d75d2554e6a556e5e13a
plojo <plonjo3@gmail.com>
2025-02-04 15:09:39 +0100

    use bufferSubData for material data updates

--- Source/Core/Render/RenderInjectorCoat.ts
+++ Source/Core/Render/RenderInjectorCoat.ts
@@ -35,7 +35,7 @@ namespace FudgeCore {
 
     protected static updateRenderData(this: Coat): void {
       const crc3: WebGL2RenderingContext = RenderWebGL.getRenderingContext();
-      const data: Float32Array = new Float32Array(4 + 1 + 1 + 1 + 1 + 1);
+      const data: Float32Array = new Float32Array(4 + 1 + 1 + 1 + 1 + 1); // vct4 color, float diffuse, float specular, float intensity, float metallic, float alphaClip
       
       if (this instanceof CoatColored) 
         data.set(this.color.get(), 0);
@@ -47,6 +47,8 @@ namespace FudgeCore {
         data[7] = this.metallic;
       }
 
+      data[8] = this.alphaClip;
+
       if (this instanceof CoatTextured) 
         this.texture.useRenderData(TEXTURE_LOCATION.COLOR.UNIT);
       
@@ -56,12 +58,15 @@ namespace FudgeCore {
       if (this instanceof CoatToon) 
         this.texToon.useRenderData(TEXTURE_LOCATION.TOON.UNIT);
 
-      data[8] = this.alphaClip;
-
       // buffer data to bound buffer
-      this.renderBuffer ??= RenderWebGL.assert(crc3.createBuffer());
+      if (!this.renderBuffer) {
+        this.renderBuffer ??= RenderWebGL.assert(crc3.createBuffer());
+        crc3.bindBuffer(WebGL2RenderingContext.UNIFORM_BUFFER, this.renderBuffer);
+        crc3.bufferData(WebGL2RenderingContext.UNIFORM_BUFFER, data.byteLength, WebGL2RenderingContext.DYNAMIC_DRAW);
+      }
+
       crc3.bindBuffer(WebGL2RenderingContext.UNIFORM_BUFFER, this.renderBuffer);
-      crc3.bufferData(WebGL2RenderingContext.UNIFORM_BUFFER, data, WebGL2RenderingContext.DYNAMIC_DRAW);
+      crc3.bufferSubData(WebGL2RenderingContext.UNIFORM_BUFFER, 0, data);
     }
 
     protected static deleteRenderData(this: Coat): void {
