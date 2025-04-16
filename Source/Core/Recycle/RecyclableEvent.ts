05f08d427bb95fa9d546c6a0afacdab9929456dc
Jonas Plotzky <plonjo3@gmail.com>
2025-04-16 11:13:40 +0200

    adjust recyclable event

--- Source/Core/Recycle/RecyclableEvent.ts
+++ Source/Core/Recycle/RecyclableEvent.ts
@@ -2,22 +2,30 @@ namespace FudgeCore {
   type Writable<T, K extends keyof T> = Omit<T, K> & { -readonly [P in K]: T[P] };
   type WritableEvent = Writable<Event, "type" | "target" | "currentTarget" | "eventPhase" | "bubbles" | "cancelable">;
 
+  const descriptor: PropertyDescriptor = { writable: true };
+  /**
+   * Attempts to make a readonly property of an object writable. Throws an error if the property is not configurable.
+   */
+  function makePropertyWritable<T>(_object: T, _key: keyof T): void {
+    descriptor.value = _object[_key];
+    Object.defineProperty(_object, _key, descriptor);
+    // delete descriptor.value;
+  }
+
   /**
    * A subclass of {@link Event} that can be reused via the {@link Recycler} to reduce the number of event objects created.
    * Exposes readonly properties of the event class as writable properties.
    * @author Jonas Plotzky, HFU, 2025
    */
   export class RecyclableEvent extends Event implements Recycable {
-    static readonly #descriptor: PropertyDescriptor = { writable: true };
-
     public constructor(_type?: string, _init?: EventInit) {
       super(_type, _init);
-      this.#makeWritable("bubbles");
-      this.#makeWritable("cancelable");
-      this.#makeWritable("currentTarget");
-      this.#makeWritable("eventPhase");
-      this.#makeWritable("target");
-      this.#makeWritable("type");
+      makePropertyWritable(this, "bubbles");
+      makePropertyWritable(this, "cancelable");
+      makePropertyWritable(this, "currentTarget");
+      makePropertyWritable(this, "eventPhase");
+      makePropertyWritable(this, "target");
+      makePropertyWritable(this, "type");
     }
 
     public static [Symbol.hasInstance](_instance: unknown): boolean { // much faster than default instanceof
@@ -82,11 +90,6 @@ namespace FudgeCore {
     public setEventPhase(this: WritableEvent, _eventPhase: Event["NONE"] | Event["CAPTURING_PHASE"] | Event["AT_TARGET"] | Event["BUBBLING_PHASE"]): RecyclableEvent {
       this.eventPhase = _eventPhase;
       return <RecyclableEvent>this;
-    }
-
-    #makeWritable(_key: keyof this): void {
-      RecyclableEvent.#descriptor.value = this[_key];
-      Object.defineProperty(this, _key, RecyclableEvent.#descriptor);
     }
   }
 }
