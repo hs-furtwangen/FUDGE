c5d0145b4995fa2b3d0e5cb0e0360b09d9135be9
plojo <plonjo3@gmail.com>
2025-06-23 17:21:33 +0200

    update typedoc

--- Source/Core/Math/ArrayConvertible.ts
+++ Source/Core/Math/ArrayConvertible.ts
@@ -3,7 +3,7 @@ namespace FudgeCore {
     readonly isArrayConvertible: true;
 
     /**
-     * Set the elements of this entity from the given array starting at the given offset.
+     * Set the values of this object from the given array starting at the given offset.
      * @param _array - The array to read the values from.
      * @param _offset - (optional) The offset to start reading from.
      * @returns A reference to this instance.
@@ -11,7 +11,7 @@ namespace FudgeCore {
     fromArray(_array: ArrayLike<number>, _offset?: number): this;
 
     /**
-     * Copy the elements of this entity into the given array starting at the given offset. Creates a new array if none is provided.
+     * Copy the values of this object into the given array starting at the given offset. Creates a new array if none is provided.
      * @param _out - (optional) The receiving array.
      * @param _offset - (optional) The offset to start writing to.
      * @returns `_out` or a new array if none is provided.
